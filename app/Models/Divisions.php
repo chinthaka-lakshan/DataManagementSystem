64eb778575ab637f11892635dde9f9bbea71cf12
LAKSHAN M.J.C.D. <diluklakshan01@gmail.com>
2026-05-01 18:42:22 +0530

    modify admin sidebar

--- app/Models/Divisions.php
+++ app/Models/Divisions.php
@@ -17,7 +17,7 @@ class Divisions extends Model
     {
         return $this->hasMany(Household::class);
     }
-    public function users()
+    public function user()
     {
         return $this->belongsTo(User::class, 'user_id');
     }
